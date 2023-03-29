44630093171f9c9ea9947b2ecd28947b1d1c0fa9
rosenhristov <rosenhrist@gmail.com>
2023-03-29 17:21:43 +0300

    Refactored Spring to work with MySQL due to problems with storing the large pics in Postgres

--- 03-backend/spring-boot-library/src/main/java/com/rosenhristov/springbootlibrary/SpringBootLibraryApplication.java
+++ 03-backend/spring-boot-library/src/main/java/com/rosenhristov/springbootlibrary/SpringBootLibraryApplication.java
@@ -2,6 +2,7 @@ package com.rosenhristov.springbootlibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootLibraryApplication {
@@ -9,4 +10,12 @@ public class SpringBootLibraryApplication {
 		SpringApplication.run(SpringBootLibraryApplication.class, args);
 	}
 
+//	@Bean
+//	public Class<?> MySQLDriver() {
+//		try {
+//			return (Class<?>) Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+//		} catch (Exception e) {
+//			throw new RuntimeException(e);
+//		}
+//	}
 }
